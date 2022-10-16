d2ebcc66f12eaa2a045a73ba680d41948a606b63
steel99xl <steel99xl@gmail.com>
2022-10-16 18:48:02 -0400

    Add files via upload

--- basiccallback.cpp
+++ basiccallback.cpp
@@ -10,26 +10,111 @@ void BasicCallBack::test(){
 
     std::cout << "UWU it workded" << std::endl;
 
-    manager = new QNetworkAccessManager(this);
 
-    connect(manager, &QNetworkAccessManager::finished, this, &BasicCallBack::replyFinished);
+    StolenImage ImageToSteal;
+    ImageToSteal.PageBuffer = BingGetter(ImageToSteal);
 
-    manager->get(QNetworkRequest(QUrl("http://qt-project.org")));
+    //wait for connection
 
+    BaseData = "";
+
+
+    ImageToSteal.Url = BingPageToWallPaper(ImageToSteal);
+    std::cout << "Getting Photo" << std::endl;
+
+    ImageToSteal.HasUrl = true;
+    ImageToSteal.FileName = BingPageToWallPaper(ImageToSteal);
+
+    ImageToSteal.PageBuffer = BingGetter(ImageToSteal);
+
+
+
+    SaveStolenImage(ImageToSteal);
+    std::cout << "Thing Done" << std::endl;
+    //std::cout << BaseData.toStdString() << std::endl;
 
 }
 
 void BasicCallBack::replyFinished(QNetworkReply *reply){
     if(reply->error() == QNetworkReply::NetworkError::NoError){
-        QString data = (QString)reply->readAll();
-        QByteArray Bdata = data.toLocal8Bit();
-        const char *output = Bdata.data();
-        std::cout << output << std::endl;
+        this->BaseData = (QString)reply->readAll();
     } else{
-        QString data = (QString)reply->errorString();
-        QByteArray Bdata = data.toLocal8Bit();
-        const char *output = Bdata.data();
-        std::cout << output << std::endl;
+        this->BaseData = (QString)reply->errorString();
+    }
+
+}
+
+
+
+
+// Yes it just gets the bing homepage...
+std::string BasicCallBack::BingGetter(StolenImage ImageToSteal){
+    std::string data;
+    char url[128];
+
+    manager = new QNetworkAccessManager(this);
+
+    manager->setAutoDeleteReplies(true);
+
+    QEventLoop loop;
+    //connect(manager, &QNetworkAccessManager::finished, this, &BasicCallBack::replyFinished);
+    if(ImageToSteal){
+        QNetworkReply *Temp = manager->get(QNetworkRequest(QUrl((QString)ImageToSteal.Url.c_str())));
+        connect(Temp, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        loop.exec();
+
+
+        data = (std::string)Temp->readAll().toStdString();
+    } else {
+        QNetworkReply *Temp = manager->get(QNetworkRequest(QUrl((QString)BingLink)));
+        connect(Temp, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        loop.exec();
+
+
+        data = (std::string)Temp->readAll().toStdString();
+    }
+
+
+
+
+
+    //data = (std::string)this->BaseData.toStdString();
+    return data;
+}
+
+std::string BasicCallBack::BingPageToWallPaper(StolenImage ImageToSteal){
+    std::string Output; // This is will be the url to the wallpaper
+
+    std::size_t StartPos = ImageToSteal.PageBuffer.find("background-image: url(")+22;
+
+    std::size_t EndPos = ImageToSteal.PageBuffer.find("&", StartPos);
+    if(ImageToSteal){
+        StartPos = ImageToSteal.PageBuffer.find(".", StartPos) + 1;
+        Output = ImageToSteal.PageBuffer.substr(StartPos, EndPos-StartPos);
+    } else {
+        Output = BingLink + ImageToSteal.PageBuffer.substr(StartPos, EndPos-StartPos);
+    }
+
+    return Output;
+
+
+}
+
+void BasicCallBack::SaveStolenImage(StolenImage ImageToSteal){
+    std::ofstream Contraband;
+
+    if(strcmp(ImageToSteal.FileName.c_str() , "") == 0){
+        Contraband.open(ImageToSteal.Url.c_str(), std::ios::out);
+    } else {
+        Contraband.open(ImageToSteal.FileName.c_str(), std::ios::out);
+    }
+
+    if(Contraband.is_open()){
+        Contraband << ImageToSteal.PageBuffer + "\n";
+        Contraband.close();
+    } else {
+        std::cout << "ERROR could not open FILE for writeing" << std::endl;
+
     }
 
 }
--- basiccallback.h
+++ basiccallback.h
@@ -3,14 +3,33 @@
 
 #include <QObject>
 #include <iostream>
+#include <cstring>
+#include <fstream>
+#include <vector>
 #include <QNetworkAccessManager>
 #include <QNetworkRequest>
 #include <QNetworkReply>
+#include <QEventLoop>
+
 
 class BasicCallBack : public QObject
 {
     Q_OBJECT
 public:
+    struct StolenImage{
+        bool HasUrl = false;
+        std::string PageBuffer = "";
+        std::string Url = "";
+        std::string FileName = "";
+
+        operator bool(){return HasUrl;}
+    };
+
+    const char *BingLink = "https://www.bing.com/";
+
+    QString BaseData;
+
+
     explicit BasicCallBack(QObject *parent = nullptr);
 
     Q_INVOKABLE void test();
@@ -19,6 +38,12 @@ signals:
 
 public slots:
     void replyFinished (QNetworkReply *reply);
+
+    std::string BingGetter(StolenImage ImageToSteal);
+
+    std::string BingPageToWallPaper(StolenImage ImageToSteal);
+
+    void SaveStolenImage(StolenImage ImageToSteal);
 
 private:
     QNetworkAccessManager *manager;
